5fe73a132815885dc472ceb0b237f8b212c70cd6
Ghasan Al-Sakkaf <devghasan@hotmail.com>
2019-11-28 19:02:38 +0800

    WIP

--- FriBidiSharp/FriBidiSharps.cs
+++ FriBidiSharp/FriBidiSharps.cs
@@ -4,33 +4,8 @@ using System.Text;
 
 namespace FriBidiSharp
 {
-    unsafe public static class Api
+    public static class FriBidiSharpExtensions
     {
-        //public static sbyte LogicalToVisual(uint[] str, int length, uint[] pbaseDir, uint[] visualStr, int[] logicalToVisualPositions, int[] visualToLogicalPositions, sbyte[] embeddingLevels)
-        //{
-        //    fixed (uint* strP = &str[0])
-        //    {
-        //        fixed (uint* pbaseDirP = &pbaseDir[0])
-        //        {
-        //            fixed (uint* visualStrP = &visualStr[0])
-        //            {
-        //                fixed (int* logicalToVisualPositionsP = &logicalToVisualPositions[0])
-        //                {
-        //                    fixed (int* visualToLogicalPositionsP = &visualToLogicalPositions[0])
-        //                    {
-        //                        fixed (sbyte* embeddingLevelsP = &embeddingLevels[0])
-        //                        {
-        //                            return fribidi_begindecls.Log2vis(strP, length, pbaseDirP, visualStrP, logicalToVisualPositionsP, visualToLogicalPositionsP, embeddingLevelsP);
-        //                        }
-        //                    }
-        //                }
-        //            }
-        //        }
-        //    }
-        //}
-
-        // Extensions
-
         public static string ToUtf16String(this int[] utf32String)
         {
             var bldr = new StringBuilder();
