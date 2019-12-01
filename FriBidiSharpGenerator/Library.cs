2169a196e9e8b3f9791a09b2217ab054de7bd2a7
Ghasan Al-Sakkaf <devghasan@hotmail.com>
2019-12-01 17:29:49 +0800

    WIP

--- FriBidiSharpGenerator/Library.cs
+++ FriBidiSharpGenerator/Library.cs
@@ -1,9 +1,14 @@
-﻿using CppSharp;
+﻿using CodingSeb.ExpressionEvaluator;
+using CppSharp;
 using CppSharp.AST;
 using CppSharp.Passes;
 using FribidiSharpGenerator;
 using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace FriBidiSharpGenerator
 {
@@ -65,6 +70,65 @@ namespace FriBidiSharpGenerator
             ctx.GenerateEnumFromMacros("Flags", "FRIBIDI_FLAG_(.*)").SetFlags();
             ctx.GenerateEnumFromMacros("NamedChar", "FRIBIDI_CHAR_(.*)");
 
+            foreach (var unit in ctx.TranslationUnits)
+            {
+                foreach (var macro in unit.PreprocessedEntities.OfType<MacroDefinition>())
+                {
+                    if (macro.Name.StartsWith("FRIBIDI") && Regex.IsMatch(macro.Expression, "(?i)^0x[0-9a-f]+L$"))
+                    {
+                        string num = macro.Expression.Substring(2);
+                        num = num.TrimEnd('L');
+                        num = UInt64.Parse(num, NumberStyles.HexNumber).ToString();
+                        macro.Expression = num;
+                    }
+                }
+            }
+
+            ctx.GenerateEnumFromMacros("TypeMasks", "FRIBIDI_MASK_(.*)").SetFlags();
+            ctx.GenerateEnumFromMacros("JoinMasks", getJoinMasks()).SetFlags();
+
+            var evaluator = new ExpressionEvaluator();
+            var tempMacros = new HashSet<MacroDefinition>();
+
+            foreach (var unit in ctx.TranslationUnits)
+            {
+                foreach (var macro in unit.PreprocessedEntities.OfType<MacroDefinition>())
+                {
+                    if (Regex.IsMatch(macro.Name, "^FRIBIDI_(TYPE|PAR)"))
+                    {
+                        var typeMasks = ctx.FindEnum("TypeMasks").Single();
+                        var joinMasks = ctx.FindEnum("JoinMasks").Single();
+
+                        var exp = macro.Expression.Replace("\\", String.Empty);
+
+                        foreach (Match refMacro in Regex.Matches(exp, @"FRIBIDI_\w+"))
+                        {
+                            var typeMask = typeMasks.Items.SingleOrDefault(i => i.Name == refMacro.Value);
+                            var joinMask = joinMasks.Items.SingleOrDefault(i => i.Name == refMacro.Value);
+                            var sibling = tempMacros.SingleOrDefault(i => i.Name == refMacro.Value);
+
+                            if (typeMask != null)
+                                exp = exp.Replace(refMacro.Value, typeMask.Value.ToString());
+                            else if (joinMask != null)
+                                exp = exp.Replace(refMacro.Value, joinMask.Value.ToString());
+                            else if (sibling != null)
+                                exp = exp.Replace(refMacro.Value, sibling.Expression);
+                            else
+                                throw new ArgumentOutOfRangeException();
+                        }
+
+                        exp = evaluator.Evaluate(exp).ToString();
+
+                        macro.Expression = exp;
+
+                        tempMacros.Add(macro);
+                    }
+                }
+            }
+
+            ctx.GenerateEnumFromMacros("Type", "FRIBIDI_TYPE_(.*)");
+            ctx.GenerateEnumFromMacros("DeprecatedType", getDeprecatedTypes());
+
             driver.Context.TranslationUnitPasses.Passes.RemoveAll(i => i.GetType() == typeof(MarshalPrimitivePointersAsRefTypePass));
         }
 
@@ -100,6 +164,33 @@ namespace FriBidiSharpGenerator
                 dir = Directory.GetParent(dir).FullName;
 
             return Path.Combine(dir, "FriBidiSharp");
+        }
+
+        private static string[] getJoinMasks()
+        {
+            return new string[]
+            {
+                "FRIBIDI_MASK_JOINS_RIGHT",
+                "FRIBIDI_MASK_JOINS_LEFT",
+                "FRIBIDI_MASK_ARAB_SHAPES",
+                "FRIBIDI_MASK_TRANSPARENT",
+                "FRIBIDI_MASK_IGNORED",
+                "FRIBIDI_MASK_LIGATURED",
+            };
+        }
+
+        private static string[] getDeprecatedTypes()
+        {
+            return new string[]
+            {
+                "FRIBIDI_PAR_WLTR",
+                "FRIBIDI_PAR_WRTL",
+                "FRIBIDI_PAR_LTR",
+                "FRIBIDI_PAR_RTL",
+                "FRIBIDI_PAR_ON",
+                "FRIBIDI_TYPE_BS",
+                "FRIBIDI_TYPE_SS",
+            };
         }
     }
 }
